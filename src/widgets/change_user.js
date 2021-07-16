feb81bf1c3b1ecae26edd9e14742d6cc8eba0751
JodliDev <jodlidev@gmail.com>
2021-07-16 08:41:15 +0200

    Improve password field for creating user

--- src/widgets/change_user.js
+++ src/widgets/change_user.js
@@ -2,18 +2,23 @@ import {Lang} from "../js/main_classes/lang";
 import ko from "knockout";
 import {close_on_clickOutside} from "../js/helpers/basics";
 
+const PASSWORD_MIN_LENGTH = 12;
+
 export function ChangeUser_viewModel(page, rootEl, params) {
 	let self = this;
 	
 	this.stayOpen = params.stayOpen || false;
 	
 	this.isOpen = ko.observable(false);
-	this.passwordValid = ko.observable(false);
 	this.password = ko.observable('');
 	this.passwordRepeat = ko.observable('');
 	//knockout wraps the username in an observable not matter what - I dont really understand why...
 	this.username = ko.observable(typeof params.username === "function" ? params.username() : params.username);
 	this.needsUsername = !this.username();
+	this.enableForm = ko.pureComputed(function() {
+		return self.password().length >= PASSWORD_MIN_LENGTH && self.password() === self.passwordRepeat();
+	});
+	this.passMsg = ko.observable();
 	
 	this.open = function() {
 		self.isOpen(true);
@@ -23,7 +28,7 @@ export function ChangeUser_viewModel(page, rootEl, params) {
 	this.save = function() {
 		if(self.needsUsername && self.username().length < 3)
 			page.loader.error(Lang.get('error_short_username'));
-		else if(self.check_badPassword(self.password()))
+		else if(self.password() < PASSWORD_MIN_LENGTH)
 			page.loader.error(Lang.get('error_bad_password'));
 		else {
 			params.finish(self.username(), self.password()).then(function() {
@@ -37,28 +42,27 @@ export function ChangeUser_viewModel(page, rootEl, params) {
 		}
 	}
 	
-	this.check_badPassword = function(pass) {
-		let l = pass.length;
-		return 3 - [l >= 4, l >= 8, l >= 12].filter(Boolean).length;
-	}
-	this.visualize_password = function(el) {
+	this.change_password = function(data, e) {
+		let el = e.target;
 		let pass = el.value;
-		if(pass.length === 0) {
-			el.style.borderBottom = "";
-			return;
-		}
-		
-		let color;
 		let l = pass.length;
-		if(l >= 12)
-			color = 'lightgreen';
-		else if(l >= 8)
-			color = 'yellow';
-		else if(l >= 4)
-			color = 'orange';
-		else
-			color = 'orangered';
 		
-		el.style.borderBottom = "3px solid "+color;
+		self.password(pass);
+		
+		if(!l) {
+			self.passMsg("");
+			el.style.outline = "0px solid transparent";
+		}
+		else if(l >= PASSWORD_MIN_LENGTH) {
+			self.passMsg("");
+			el.style.outline = "3px solid lightgreen";
+		}
+		else {
+			self.passMsg(Lang.get("minimal_length", PASSWORD_MIN_LENGTH));
+			el.style.outline = "3px solid orangered";
+		}
+	}
+	this.change_passwordRepeat = function(data, e) {
+		self.passwordRepeat(e.target.value);
 	}
 }
