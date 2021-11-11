3f623dd34d5e54c0ab4b652dd22cda8e856d7237
JodliDev <jodlidev@gmail.com>
2021-11-11 09:36:30 +0100

    fix autoload bug in linux

--- src/backend/autoload.php
+++ src/backend/autoload.php
@@ -1,6 +1,7 @@
 <?php
 define('DIR_BASE', dirname(dirname(__FILE__)) .'/');
 function autoloader($class) {
+	$class = str_replace('\\', '/', $class);
 	include DIR_BASE . "$class.php";
 }
 spl_autoload_register('autoloader');
