28ac88674d16e4999bcbc418bf00c93c65369069
JodliDev <jodlidev@gmail.com>
2021-12-10 13:50:11 +0100

    fix: load from default configs if key does not exist in local configs

--- src/backend/Configs.php
+++ src/backend/Configs.php
@@ -8,20 +8,27 @@ class Configs {
 	private static $configs = null;
 	
 	static function get($key) {
-		return self::getAll()[$key];
+		$current = self::getAll();
+		if(isset($current[$key]))
+			return $current[$key];
+		else {
+			$default = self::getDefaultAll();
+			return isset($default[$key]) ? $default[$key] : null;
+		}
 	}
 	static function getAll() {
 		if(self::$configs)
 			return self::$configs;
 		else if(file_exists(Files::FILE_CONFIG))
 			return self::$configs = require Files::FILE_CONFIG;
-		else if(file_exists(Files::FILE_DEFAULT_CONFIG))
+		else
+			return self::getDefaultAll();
+	}
+	static function getDefaultAll() {
+		if(file_exists(Files::FILE_DEFAULT_CONFIG))
 			return require Files::FILE_DEFAULT_CONFIG;
 		else
 			return [];
-	}
-	static function getDefaultAll() {
-		return require Files::FILE_DEFAULT_CONFIG;
 	}
 	
 	static function get_serverName() {
