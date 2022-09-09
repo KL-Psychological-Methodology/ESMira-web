b46c0a5cf968fa1435988765a2e9eaa1a196190d
JodliDev <jodlidev@gmail.com>
2022-09-09 09:24:32 +0200

    Fix UpdateVersion.php to consider that old classes are still loaded when script is run

--- src/backend/admin/features/adminPermission/UpdateVersion.php
+++ src/backend/admin/features/adminPermission/UpdateVersion.php
@@ -9,6 +9,7 @@ use backend\dataClasses\Message;
 use backend\dataClasses\StudyStatisticsEntry;
 use backend\dataClasses\UserData;
 use backend\DataSetCacheStatisticsEntry;
+use backend\fileSystem\DataStoreFS;
 use backend\fileSystem\loader\ErrorReportInfoLoader;
 use backend\fileSystem\loader\MessagesArchivedLoader;
 use backend\fileSystem\loader\MessagesPendingLoader;
@@ -22,6 +23,7 @@ use backend\PageFlowException;
 use backend\Paths;
 use backend\ResponsesIndex;
 use stdClass;
+use Throwable;
 
 class UpdateVersion extends CheckUpdate {
 	
@@ -39,6 +41,7 @@ class UpdateVersion extends CheckUpdate {
 			]);
 		}
 		if($fromVersion <= 200) {
+			require_once DIR_BASE .'backend/exceptions.php';
 			$changeResponsesIndex = function(int $studyId, string $identifier) {
 				$values = unserialize(file_get_contents(PathsFS::fileResponsesIndex($studyId, $identifier)));
 				if(!$values)
@@ -107,118 +110,132 @@ class UpdateVersion extends CheckUpdate {
 			closedir($handle);
 			
 			
-			
-			$studyStore = Configs::getDataStore()->getStudyStore();
-			$messagesStore = Configs::getDataStore()->getMessagesStore();
+			//php has still loaded the old Configs so we have to get our store class manually
+			$dataStore = new DataStoreFS();
+			$studyStore = $dataStore->getStudyStore();
+			$messagesStore = $dataStore->getMessagesStore();
 			
 			foreach($studyStore->getStudyIdList() as $studyId) {
-				//
-				//replace format in PathsFS::fileStatisticsNewData() from "a|b|c" to [a,b,c]
-				//
-				
-				$path = PathsFS::fileStatisticsNewData($studyId);
-				if(file_exists($path)) {
-					$newDataCollection = file($path);
+				try {
+					//
+					//replace format in PathsFS::fileStatisticsNewData() from "a|b|c" to [a,b,c]
+					//
 					
-					$newContent = '';
-					foreach($newDataCollection as $line) {
-						$newData = explode('|', trim($line));
-						if(count($newData) != 4) { //skip if line is empty or data is already in new format
-							$newContent .= "\n" .trim($line);
-							continue;
+					$path = PathsFS::fileStatisticsNewData($studyId);
+					if(file_exists($path)) {
+						$newDataCollection = file($path);
+						
+						$newContent = '';
+						foreach($newDataCollection as $line) {
+							$newData = explode('|', trim($line));
+							if(count($newData) != 4) { //skip if line is empty or data is already in new format
+								$newContent .= "\n" . trim($line);
+								continue;
+							}
+							list($key, $index, $timestamp, $answer) = $newData;
+							$entry = new DataSetCacheStatisticsEntry($key, $index, $timestamp, $answer);
+							$newContent .= "\n" . StatisticsNewDataSetEntryLoader::export($entry);
 						}
-						list($key, $index, $timestamp, $answer) = $newData;
-						$entry = new DataSetCacheStatisticsEntry($key, $index, $timestamp, $answer);
-						$newContent .= "\n" .StatisticsNewDataSetEntryLoader::export($entry);
+						file_put_contents($path, $newContent, LOCK_EX);
 					}
-					file_put_contents($path, $newContent, LOCK_EX);
-				}
-				
-				
-				//
-				//replace format in PathsFS::fileStudyStatisticsMetadata() from {stdClass} to [StudyStatisticsEntry]
-				//
-				
-				$path = PathsFS::fileStudyStatisticsMetadata($studyId);
-				$oldMetadata = file_exists($path)
-					? unserialize(file_get_contents($path))
-					: new stdClass();
-				$newMetadata = [];
-				
-				//these loops are also compatible with the new format:
-				foreach($oldMetadata as $key => $metadataCollection) {
-					$newMetadata[$key] = [];
-					foreach($metadataCollection as $i => $metadata) {
-						$newMetadata[$key][$i] = new StudyStatisticsEntry(
-							$metadata->conditions,
-							$metadata->conditionType,
-							$metadata->storageType,
-							$metadata->defaultTimeInterval ?? $metadata->timeInterval
-						);
+					
+					
+					//
+					//replace format in PathsFS::fileStudyStatisticsMetadata() from {stdClass} to [StudyStatisticsEntry]
+					//
+					
+					$path = PathsFS::fileStudyStatisticsMetadata($studyId);
+					$oldMetadata = file_exists($path)
+						? unserialize(file_get_contents($path))
+						: new stdClass();
+					$newMetadata = [];
+					
+					//these loops are also compatible with the new format:
+					foreach($oldMetadata as $key => $metadataCollection) {
+						$newMetadata[$key] = [];
+						foreach($metadataCollection as $i => $metadata) {
+							$newMetadata[$key][$i] = new StudyStatisticsEntry(
+								$metadata->conditions,
+								$metadata->conditionType,
+								$metadata->storageType,
+								$metadata->defaultTimeInterval ?? $metadata->timeInterval
+							);
+						}
+					}
+					FileSystemBasics::writeFile($path, serialize($newMetadata));
+					
+					
+					//
+					//replace format for responsesIndex from [[], []] to ResponsesIndex
+					//
+					
+					$study = $studyStore->getStudyConfig($studyId);
+					foreach($study->questionnaires as $questionnaire) {
+						$changeResponsesIndex($studyId, (string)$questionnaire->internalId);
+					}
+					$changeResponsesIndex($studyId, PathsFS::FILENAME_EVENTS);
+					$changeResponsesIndex($studyId, PathsFS::FILENAME_WEB_ACCESS);
+					
+					
+					//
+					//replace format for userData from AArray to UserData
+					//
+					foreach($studyStore->getStudyParticipants($studyId) as $userId) {
+						try {
+							$path = PathsFS::fileUserData($studyId, $userId);
+							$array = unserialize(file_get_contents($path));
+							
+							if(!$array)
+								throw new CriticalError("Could not unserialize userdata $userId for study $studyId");
+							if(!is_array($array))//skip if data is already in new format
+								continue;
+							
+							$userdata = new UserData(
+								$array['userId'] ?? -1,
+								$array['token'] ?? '',
+									(int) ($array['dataSetCount'] ?? 0),
+								$array['group'] ?? 0,
+								$array['appType'] ?? '',
+								$array['appVersion'] ?? '0'
+							);
+							file_put_contents($path, UserDataLoader::export($userdata));
+						}
+						catch(Throwable $e) {
+							throw new PageFlowException("Failed updating user data for $userId", 0, $e);
+						}
+					}
+					
+					
+					//
+					//replace format for messages from [AArray] to [Message]
+					//
+					
+					foreach($messagesStore->getParticipantsWithMessages($studyId) as $msgInfo) {
+						try {
+							$userId = $msgInfo->name;
+							MessagesArchivedLoader::exportFile(
+								$studyId,
+								$userId,
+								$changeMessageFormat(PathsFS::fileMessageArchive($studyId, $userId))
+							);
+							MessagesPendingLoader::exportFile(
+								$studyId,
+								$userId,
+								$changeMessageFormat(PathsFS::fileMessagePending($studyId, $userId))
+							);
+							MessagesUnreadLoader::exportFile(
+								$studyId,
+								$userId,
+								$changeMessageFormat(PathsFS::fileMessageUnread($studyId, $userId))
+							);
+						}
+						catch(Throwable $e) {
+							throw new PageFlowException("Failed updating messages for $userId", 0, $e);
+						}
 					}
 				}
-				FileSystemBasics::writeFile($path, serialize($newMetadata));
-				
-				
-				//
-				//replace format for responsesIndex from [[], []] to ResponsesIndex
-				//
-				
-				$study = $studyStore->getStudyConfig($studyId);
-				foreach($study->questionnaires as $questionnaire) {
-					$changeResponsesIndex($studyId, (string) $questionnaire->internalId);
-				}
-				$changeResponsesIndex($studyId, PathsFS::FILENAME_EVENTS);
-				$changeResponsesIndex($studyId, PathsFS::FILENAME_WEB_ACCESS);
-				
-				
-				//
-				//replace format for userData from AArray to UserData
-				//
-				
-				foreach($studyStore->getStudyParticipants($studyId) as $userId) {
-					$path = PathsFS::fileUserData($studyId, $userId);
-					$array = unserialize(file_get_contents($path));
-					
-					if(!$array)
-						throw new CriticalError("Could not unserialize userdata $userId for study $studyId");
-					if(!is_array($array))//skip if data is already in new format
-						continue;
-					
-					$userdata = new UserData(
-						$array['userId'] ?? -1,
-						$array['token'] ?? '',
-						$array['dataSetCount'] ?? 0,
-						$array['group'] ?? 0,
-						$array['appType'] ?? '',
-						$array['appVersion'] ?? '0'
-					);
-					file_put_contents($path, UserDataLoader::export($userdata));
-					
-				}
-				
-				
-				//
-				//replace format for messages from [AArray] to [Message]
-				//
-				
-				foreach($messagesStore->getParticipantsWithMessages($studyId) as $msgInfo) {
-					$userId = $msgInfo->name;
-					MessagesArchivedLoader::exportFile(
-						$studyId,
-						$userId,
-						$changeMessageFormat(PathsFS::fileMessageArchive($studyId, $userId))
-					);
-					MessagesPendingLoader::exportFile(
-						$studyId,
-						$userId,
-						$changeMessageFormat(PathsFS::fileMessagePending($studyId, $userId))
-					);
-					MessagesUnreadLoader::exportFile(
-						$studyId,
-						$userId,
-						$changeMessageFormat(PathsFS::fileMessageUnread($studyId, $userId))
-					);
+				catch(Throwable $e) {
+					throw new PageFlowException("Failed updating study $studyId\n$e", 0, $e);
 				}
 			}
 		}
