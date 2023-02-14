83604d098e9c025cfc490501b040fa45910c14a0
JodliDev <jodlidev@gmail.com>
2023-02-14 08:07:02 +0100

    Do not treat columns called "date" as Date (obviously)

--- src/frontend/js/variables/csv.js
+++ src/frontend/js/variables/csv.js
@@ -1,3 +1,3 @@
 export const CSV_DELIMITER = ";",
 	DATAVIEWER_SKIPPED_COLUMNS = ["responseTime_formatted", "last_notification_formatted", "notification_scheduled_formatted"],
-	DATAVIEWER_TIMESTAMP_COLUMNS = ["date", "uploaded", "responseTime", "last_notification", "notification_scheduled"];
+	DATAVIEWER_TIMESTAMP_COLUMNS = ["uploaded", "responseTime", "last_notification", "notification_scheduled"];
