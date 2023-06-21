f8d7c28cdabad44cdd6edc7c02b4d230f6c6d912
JodliDev <jodlidev@gmail.com>
2023-06-21 07:54:51 +0200

    Correct study count in server statistics on version update.

--- src/backend/admin/features/adminPermission/UpdateVersion.php
+++ src/backend/admin/features/adminPermission/UpdateVersion.php
@@ -341,6 +341,26 @@ class UpdateVersion extends DoUpdate {
 				]);
 			}
 		}
+		if($this->versionIsBelowThen('2.2.4-alpha.10')) {
+			$pathServerStatistics = PathsFS::fileServerStatistics();
+			if(file_exists($pathServerStatistics)) {
+				$handle = fopen($pathServerStatistics, 'r+');
+				$statistics = json_decode(fread($handle, filesize($pathServerStatistics)));
+				
+				$studyStore = Configs::getDataStore()->getStudyStore();
+				
+				$statistics->total->studies = 0;
+				
+				foreach($studyStore->getStudyIdList() as $studyId) {
+					$study = $studyStore->getStudyConfig($studyId);
+					if($study->published ?? false)
+						$statistics->total->studies += 1;
+				}
+				if(fseek($handle, 0) != -1 && ftruncate($handle, 0) && fwrite($handle, json_encode($statistics)))
+					fflush($handle);
+				fclose($handle);
+			}
+		}
 	}
 	
 	/**
