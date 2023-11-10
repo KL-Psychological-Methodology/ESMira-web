af382fffad794aa69bf982bb33e32c4a62d845b3
JodliDev <jodlidev@gmail.com>
2023-11-10 17:10:22 +0100

    Avoid StudyMetadataStoreFS in case old version is still in Zend cache

--- src/backend/admin/updateScripts/3.0.0-alpha.14.php
+++ src/backend/admin/updateScripts/3.0.0-alpha.14.php
@@ -1,11 +1,35 @@
 <?php
 
 use backend\Configs;
+use backend\fileSystem\PathsFS;
+use backend\FileSystemBasics;
+use backend\Permission;
+
+//Note: PathsFS::fileStudyCreateMetadata did not exist in the last version and if the server is using Zend, when this is executed, the old PathsFS will be loaded.
+// So we can not use StudyMetadataStoreFS::updateMetadata and save everything manually instead. From now on we call opcache_reset(), so this should not be necessary anymore.
 
 $studyStore = Configs::getDataStore()->getStudyStore();
 $studies = $studyStore->getStudyIdList();
 foreach($studies as $studyId) {
 	$study = $studyStore->getStudyConfig($studyId);
-	$studyMetadataStore = Configs::getDataStore()->getStudyMetadataStore($studyId);
-	$studyMetadataStore->updateMetadata($study);
+	
+	$metadata =  [
+		'version' => (int) ($study->version ?? 0),
+		'published' => $study->published ?? false,
+		'hasQuestionnaires' => isset($study->questionnaires) && count($study->questionnaires),
+		'title' => $study->title ?? 'Error',
+		'accessKeys' => $study->accessKeys ?? [],
+		'lastSavedBy' => Permission::getAccountName()
+	];
+	FileSystemBasics::writeFile(PathsFS::fileStudyMetadata($this->studyId), serialize($metadata));
+
+	$createMetadata = [
+		'timestamp' => time(),
+		'owner' => Permission::getAccountName()
+	];
+	FileSystemBasics::writeFile(PathsFS::folderStudies()."$studyId/.create_metadata", serialize($createMetadata));
+	
+	//Cannot use this:
+//	$studyMetadataStore = Configs::getDataStore()->getStudyMetadataStore($studyId);
+//	$studyMetadataStore->updateMetadata($study);
 }
