10620d55ebbf958c65b255802fa89d19182d7675
JodliDev <jodlidev@gmail.com>
2023-11-21 13:42:01 +0100

    Fix wrong permission check

--- src/backend/admin/features/loggedIn/GetFullStudy.php
+++ src/backend/admin/features/loggedIn/GetFullStudy.php
@@ -3,11 +3,12 @@
 namespace backend\admin\features\loggedIn;
 
 use backend\admin\HasWritePermission;
+use backend\admin\IsLoggedIn;
 use backend\Configs;
 use backend\exceptions\CriticalException;
 use backend\JsonOutput;
 
-class GetFullStudy extends HasWritePermission {
+class GetFullStudy extends IsLoggedIn {
 	public function execAndOutput() {
 		$studyStore = Configs::getDataStore()->getStudyStore();
 		echo JsonOutput::successString('{"config": ' .$studyStore->getStudyConfigAsJson($this->studyId) .', "languages": ' .$studyStore->getAllLangConfigsAsJson($this->studyId) .'}');
