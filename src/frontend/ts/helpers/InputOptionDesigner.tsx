6e810324852d1b8173588d89f5c15a52485869bf
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2024-03-26 10:13:26 +0100

    Modified input options for location item

--- src/frontend/ts/helpers/InputOptionDesigner.tsx
+++ src/frontend/ts/helpers/InputOptionDesigner.tsx
@@ -100,7 +100,8 @@ export class InputOptionDesigner {
 			notCompatible: ["Web"],
 			view: () => [
 				<div>
-					{ this.requiredOption() }
+					{this.requiredOption()}
+					{this.showValueOption()}
 				</div>,
 				<div>
 					<label class="vertical noDesc">
@@ -223,14 +224,19 @@ export class InputOptionDesigner {
 		},
 		"location": {
 			title: Lang.get("input_location"),
-			helpUrl: "",
+			helpUrl: "https://github.com/KL-Psychological-Methodology/ESMira/wiki/Questionnaire-Items#location-item",
 			category: "classic",
+			notCompatible: ["Web"],
 			view: () => [
 				<div>
 					{this.requiredOption()}
 				</div>,
 				<div>
-					{this.inputOptionElement(this.input.resolution, Lang.get("input_location_resolution"))}
+					<label class="spacingTop">
+						<small>{Lang.get("input_location_resolution")}</small>
+						<input type="number" { ... BindObservable(this.input.resolution) }/>
+						<small>{Lang.get("input_location_resolution_desc")}</small>
+					</label>
 				</div>
 			]
 		},
@@ -250,7 +256,18 @@ export class InputOptionDesigner {
 			title: Lang.get("input_photo"),
 			helpUrl: "https://github.com/KL-Psychological-Methodology/ESMira/wiki/Questionnaire-Items#Take-a-picture",
 			category: "media",
-			view: () => [ this.requiredOption() ]
+			view: () => [ 
+				<div>
+					{this.requiredOption()}
+				</div>,
+				<div>
+					<label class="spacingTop">
+						<small>{Lang.get("image_input_quality")}</small>
+						<input type="number" { ... BindObservable(this.input.quality) }/>
+						<small>{Lang.get("image_input_quality_desc")}</small>
+					</label>
+				</div>
+			]
 		},
 		"record_audio": {
 			title: Lang.get("input_record_audio"),
