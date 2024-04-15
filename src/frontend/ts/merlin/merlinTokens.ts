9c93645559d6f5d861f5cccd7bc92358084b585f
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2024-04-15 16:53:48 +0200

    created scanner for merlin

---
+++ src/frontend/ts/merlin/merlinTokens.ts
@@ -0,0 +1,65 @@
+class MerlinToken {
+    type: MerlinTokenType
+    lexeme: string
+    line: number
+
+    constructor(type: MerlinTokenType, lexeme: string, line: number) {
+        this.type = type
+        this.lexeme = lexeme
+        this.line = line
+    }
+}
+
+enum MerlinTokenType {
+    // Single character tokens
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    LEFT_BRACKET,
+    RIGHT_BRACKET,
+    COMMA,
+    COLON,
+    SEMICOLON,
+    DOT,
+    MINUS,
+    PLUS,
+    SLASH,
+    STAR,
+
+    // One and two character tokens
+    DOT_DOT,
+    EXCLAMATION,
+    EXCLAMATION_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+    GREATER_GREATER,
+
+    // Literals
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    // Keywords
+    AND,
+    ELSE,
+    ELIF,
+    FALSE,
+    FUNCTION,
+    FOR,
+    IF,
+    IN,
+    INIT,
+    NONE,
+    OBJECT,
+    OR,
+    RETURN,
+    TRUE,
+    WHILE,
+
+    EOF
+}
