3c87a5a10640facd8c732505dc4ea6455f7a0fd2
Selina <57713712+SelinaDev@users.noreply.github.com>
2024-09-30 10:57:31 +0200

    revert update script to not use backup manager

--- src/backend/admin/features/adminPermission/DoUpdate.php
+++ src/backend/admin/features/adminPermission/DoUpdate.php
@@ -3,7 +3,6 @@
 namespace backend\admin\features\adminPermission;
 
 use backend\admin\HasAdminPermission;
-use backend\BackupManager;
 use backend\exceptions\CriticalException;
 use backend\Paths;
 use backend\FileSystemBasics;
@@ -12,6 +11,9 @@ use Throwable;
 use ZipArchive;
 
 class DoUpdate extends HasAdminPermission {
+	//we dont want to blindly copy everything over in case there are non ESMira-files in our main folder:
+	const NEEDS_BACKUP = ['api', 'backend',  'frontend', 'locales', '.htaccess', 'CHANGELOG.md', 'index.php', 'index_nojs.php', 'LICENSE', 'README.md'];
+	
 	/**
 	 * @var string
 	 */
@@ -19,9 +21,13 @@ class DoUpdate extends HasAdminPermission {
 	/**
 	 * @var string
 	 */
+	protected $folderPathBackup;
+	/**
+	 * @var string
+	 */
 	protected $fileUpdate;
 	
-	private $backupManager;
+	private $filesToRetain = [];
 	
 	public function __construct(
 		string $folderPathSource = DIR_BASE,
@@ -31,8 +37,8 @@ class DoUpdate extends HasAdminPermission {
 		parent::__construct();
 		//we define them here so we can change the location for testing:
 		$this->folderPathSource = $folderPathSource;
+		$this->folderPathBackup = $folderPathBackup;
 		$this->fileUpdate = $fileUpdate;
-		$this->backupManager = new BackupManager($folderPathSource, $folderPathBackup);
 	}
 	
 	/**
@@ -43,20 +49,105 @@ class DoUpdate extends HasAdminPermission {
 		if(file_exists($this->fileUpdate))
 			unlink($this->fileUpdate);
 		
-		return $this->backupManager->revertFromBackup($msg);
+		$revertFailedList = [];
+		
+		//now, copy everything back from the backup folder:
+		if(file_exists($this->folderPathBackup)) {
+			$handle = opendir($this->folderPathBackup);
+			while($file = readdir($handle)) {
+				if($file == '.' || $file == '..')
+					continue;
+				
+				$oldLocation = $this->folderPathBackup . $file;
+				$newLocation = $this->folderPathSource . $file;
+				
+				//source contains the files from the update. So remove them first:
+				if(file_exists($newLocation)) {
+					if(is_file($newLocation))
+						unlink($newLocation);
+					else {
+						FileSystemBasics::emptyFolder($newLocation);
+						rmdir($newLocation);
+					}
+				}
+				
+				//Now we move stuff back:
+				if(!@rename($oldLocation, $newLocation))
+					$revertFailedList[] = $newLocation;
+			}
+			closedir($handle);
+		}
+		
+		if(count($revertFailedList))
+			throw new PageFlowException("Reverting update failed! The following files are still in the backup folder: $revertFailedList\nReverting was caused by this error: \n$msg");
+		else
+			rmdir($this->folderPathBackup);
+		
+		return new PageFlowException($msg);
+	}
+	
+	
+	/**
+	 * Windows throws some weird permission denied exceptions if we try to move the api-folder (probably because it is "used" by the server. So we move the files one by one.
+	 * @throws PageFlowException
+	 * @throws CriticalException
+	 */
+	private function move(string $oldLocation, string $newLocation) {
+		if(is_file($oldLocation)) {
+			if(!rename($oldLocation, $newLocation))
+				throw $this->revertUpdate("Renaming $oldLocation to $newLocation failed. Reverting...");
+		}
+		else {
+			$handle = opendir($oldLocation);
+			while($file = readdir($handle)) {
+				if($file == '.' || $file == '..')
+					continue;
+				
+				if(!file_exists($newLocation))
+					mkdir($newLocation, 0744);
+				$this->move("$oldLocation/$file", "$newLocation/$file");
+			}
+			closedir($handle);
+		}
+	}
+	
+	/**
+	 * @throws PageFlowException
+	 * @throws CriticalException
+	 */
+	private function moveEverythingToBackupLocation() {
+		if(!file_exists($this->folderPathBackup))
+			FileSystemBasics::createFolder($this->folderPathBackup);
+		
+		foreach(self::NEEDS_BACKUP as $file) {
+			$oldLocation = $this->folderPathSource .$file;
+			$newLocation = $this->folderPathBackup .$file;
+			
+			if(!file_exists($oldLocation))
+				continue;
+			if(file_exists($newLocation))
+				throw $this->revertUpdate("Critical error! $newLocation already exists. This should never happen. Please check the file structure manually");
+			
+			
+			$this->move($oldLocation, $newLocation);
+		}
+		
+		//remember non-ESMira files we want to keep in case we need to revert
+		$handle = opendir($this->folderPathSource);
+		while($file = readdir($handle)) {
+			$this->filesToRetain[] = $file;
+		}
+		closedir($handle);
 	}
 	
 	function exec(): array {
 		if(!file_exists($this->fileUpdate))
 			throw new PageFlowException('Could not find update. Has it been downloaded yet?');
-		if($this->backupManager->backupExists())
-			throw new PageFlowException("A backup seems to already exist");
+		if(file_exists($this->folderPathBackup))
+			throw new PageFlowException("A backup seems to already exist at: $this->folderPathBackup");
 		
-		try{
-			$this->backupManager->moveEverythingToBackupLocation(false);
-		} catch(string $m) {
-			throw $this->revertUpdate($m);
-		}
+		
+		$this->moveEverythingToBackupLocation();
 		
 		try {
 			//unpacking update:
@@ -69,7 +160,7 @@ class DoUpdate extends HasAdminPermission {
 			@unlink($this->fileUpdate);
 			
 			//restore config file:
-			if(!$this->backupManager->restoreConfig())
+			if(!@copy($this->folderPathBackup . Paths::SUB_PATH_CONFIG, Paths::FILE_CONFIG))
 				throw $this->revertUpdate('Could not restore settings. Reverting...');
 			FileSystemBasics::writeServerConfigs([]); //copies values over from the new default configs
 			if(function_exists('opcache_reset')) //for servers using Zend bytecode cache
