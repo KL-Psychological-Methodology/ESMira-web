e587de0794a1c86f5ce3e33ea489d9c6ef3da1ac
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2025-06-23 15:00:30 +0200

    fixed a bug preventing the upolad of multiple files in the same questionnaire

--- src/backend/DataSetCache.php
+++ src/backend/DataSetCache.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace backend;
@@ -6,81 +7,82 @@ namespace backend;
 use backend\exceptions\CriticalException;
 
 class DataSetCache {
-	/**
-	 * @var DataSetCacheContainer[]
-	 */
-	private $statisticsCache = [];
-	/**
-	 * @var DataSetCacheContainer[]
-	 */
-	private $eventCache = [];
-	/**
-	 * @var DataSetCacheContainer[]
-	 */
-	private $questionnaireCache = [];
-	
-	/**
-	 * @var ResponsesIndex[]
-	 */
-	private $eventIndexCache = [];
-	/**
-	 * @var ResponsesIndex[]
-	 */
-	private $questionnaireIndexCache = [];
-	
-	private $fileCache = [];
-	
-	private function addToCache(array &$cache, int $key, int $datasetId, /*mixed*/ $data) {
-		if(isset($cache[$key]))
-			$cache[$key]->add($datasetId, $data);
-		else
-			$cache[$key] = new DataSetCacheContainer($datasetId, $data);
-	}
-	
-	public function addToFileCache(int $studyId, string $internalpath, int $identifier, int $datasetId) {
-		$this->fileCache[$datasetId] = new DataSetCacheFileEntry($studyId, $internalpath, $identifier);
-	}
-	
-	public function addToStatisticsCache(int $studyId, int $datasetId, DataSetCacheStatisticsEntry $data) {
-		$this->addToCache($this->statisticsCache, $studyId, $datasetId, $data);
-	}
-	public function addToEventCache(int $studyId, int $datasetId, array $data) {
-		$this->addToCache($this->eventCache, $studyId, $datasetId, $data);
-	}
-	public function addToQuestionnaireCache(int $studyId, int $questionnaireId, int $datasetId, array $data) {
-		if(!isset($this->questionnaireCache[$studyId]))
-			$this->questionnaireCache[$studyId] = [];
-		
-		$this->addToCache($this->questionnaireCache[$studyId], $questionnaireId, $datasetId, $data);
-	}
-	
-	/**
-	 * @throws CriticalException
-	 */
-	public function getEventIndex(int $studyId): ResponsesIndex {
-		return $this->eventIndexCache[$studyId]
-			?? ($this->eventIndexCache[$studyId] = Configs::getDataStore()->getStudyStore()->getEventIndex($studyId));
-	}
-	
-	/**
-	 * @throws CriticalException
-	 */
-	public function getQuestionnaireIndex(int $studyId, int $questionnaireId): ResponsesIndex {
-		return $this->questionnaireIndexCache[$studyId .$questionnaireId]
-			?? ($this->questionnaireIndexCache[$studyId .$questionnaireId] = Configs::getDataStore()->getStudyStore()->getQuestionnaireIndex($studyId, $questionnaireId));
-	}
-	
-	public function getStatisticsCache(): array {
-		return $this->statisticsCache;
-	}
-	public function getEventCache(): array {
-		return $this->eventCache;
-	}
-	public function getQuestionnaireCache(): array {
-		return $this->questionnaireCache;
-	}
-	
-	function getFileCache(): array {
-		return $this->fileCache;
-	}
+    /**
+     * @var DataSetCacheContainer[]
+     */
+    private $statisticsCache = [];
+    /**
+     * @var DataSetCacheContainer[]
+     */
+    private $eventCache = [];
+    /**
+     * @var DataSetCacheContainer[]
+     */
+    private $questionnaireCache = [];
+    
+    /**
+     * @var ResponsesIndex[]
+     */
+    private $eventIndexCache = [];
+    /**
+     * @var ResponsesIndex[]
+     */
+    private $questionnaireIndexCache = [];
+    
+    private $fileCache = [];
+    
+    private function addToCache(array &$cache, int $key, int $datasetId, /*mixed*/ $data) {
+        if (isset($cache[$key]))
+            $cache[$key]->add($datasetId, $data);
+        else $cache[$key] = new DataSetCacheContainer($datasetId, $data);
+    }
+    
+    public function addToFileCache(int $studyId, string $internalpath, int $identifier, int $datasetId) {
+        if (!isset($this->fileCache[$datasetId]))
+            $this->fileCache[$datasetId] = [];
+        $this->fileCache[$datasetId][] = new DataSetCacheFileEntry($studyId, $internalpath, $identifier);
+    }
+    
+    public function addToStatisticsCache(int $studyId, int $datasetId, DataSetCacheStatisticsEntry $data) {
+        $this->addToCache($this->statisticsCache, $studyId, $datasetId, $data);
+    }
+    public function addToEventCache(int $studyId, int $datasetId, array $data) {
+        $this->addToCache($this->eventCache, $studyId, $datasetId, $data);
+    }
+    public function addToQuestionnaireCache(int $studyId, int $questionnaireId, int $datasetId, array $data) {
+        if (!isset($this->questionnaireCache[$studyId]))
+            $this->questionnaireCache[$studyId] = [];
+        
+        $this->addToCache($this->questionnaireCache[$studyId], $questionnaireId, $datasetId, $data);
+    }
+    
+    /**
+     * @throws CriticalException
+     */
+    public function getEventIndex(int $studyId): ResponsesIndex {
+        return $this->eventIndexCache[$studyId]
+            ?? ($this->eventIndexCache[$studyId] = Configs::getDataStore()->getStudyStore()->getEventIndex($studyId));
+    }
+    
+    /**
+     * @throws CriticalException
+     */
+    public function getQuestionnaireIndex(int $studyId, int $questionnaireId): ResponsesIndex {
+        return $this->questionnaireIndexCache[$studyId .$questionnaireId]
+            ?? ($this->questionnaireIndexCache[$studyId .$questionnaireId] = Configs::getDataStore()->getStudyStore()->getQuestionnaireIndex($studyId, $questionnaireId));
+    }
+    
+    public function getStatisticsCache(): array {
+        return $this->statisticsCache;
+    }
+    public function getEventCache(): array {
+        return $this->eventCache;
+    }
+    public function getQuestionnaireCache(): array {
+        return $this->questionnaireCache;
+    }
+    
+    function getFileCache(): array {
+        return $this->fileCache;
+    }
 }
--- src/backend/fileSystem/subStores/ResponsesStoreFS.php
+++ src/backend/fileSystem/subStores/ResponsesStoreFS.php
@@ -15,215 +15,216 @@ use backend\subStores\ResponsesStore;
 use ZipArchive;
 
 class ResponsesStoreFS implements ResponsesStore {
-	private function fillDatasetErrors(DataSetCacheContainer $entry, $msg, callable $errorCallback) {
-		foreach($entry->ids as $datasetId) {
-			$errorCallback($datasetId, $msg);
-		}
-	}
-	private function writeDataSetFile(string $path, string $text, DataSetCacheContainer $entry, callable $successCallback, callable $errorCallback) {
-		if(file_put_contents($path, $text, FILE_APPEND | LOCK_EX)) {
-			foreach($entry->ids as $datasetId) {
-				$successCallback($datasetId);
-			}
-		}
-		else {
-			Main::report("Could not write to file '$path'. Sending error response to app.");
-			$this->fillDatasetErrors($entry, 'Internal Server Error: Saving failed', $errorCallback);
-		}
-	}
-	private function writeCsv(
-		string $path,
-		string $noPathMessage,
-		DataSetCacheContainer $entry,
-		string $csvSeparator,
-		callable $successCallback,
-		callable $errorCallback
-	) {
-		if(!file_exists($path)) {
-			$this->fillDatasetErrors($entry, $noPathMessage, $errorCallback);
-			return;
-		}
-		
-		$writeString = '';
-		foreach($entry->data as $data) {
-			$writeString .= "\n".Main::arrayToCSV($data, $csvSeparator);
-		}
-		
-		$this->writeDataSetFile($path, $writeString, $entry, $successCallback, $errorCallback);
-	}
-	
-	public function saveWebAccessDataSet(int $studyId, int $timestamp, string $pageName, string $referer, string $userAgent): bool {
-		return file_put_contents(
-				PathsFS::fileResponses($studyId, PathsFS::FILENAME_WEB_ACCESS),
-				"\n\"".$timestamp."\";\"$pageName\";\"$referer\";\"$userAgent\"",
-				FILE_APPEND | LOCK_EX
-			) !== false;
-	}
-	public function saveDataSetCache(string $userId, DataSetCache $cache, callable $successProgressCallback, callable $errorProgressCallback) {
-		foreach($cache->getStatisticsCache() as $studyId => $entry) {
-			$pathStatisticsNewData = PathsFS::fileStatisticsNewData($studyId);
-			
-			$writeString = '';
-			foreach($entry->data as $data) {
-				$writeString .= "\n" .StatisticsNewDataSetEntryLoader::export($data);
-			}
-			
-			$this->writeDataSetFile($pathStatisticsNewData, $writeString, $entry, $successProgressCallback, $errorProgressCallback);
-		}
-		
-		$csvSeparator = Configs::get('csv_delimiter');
-		foreach($cache->getQuestionnaireCache() as $studyId => $questionnaireEntries) {
-			foreach($questionnaireEntries as $questionnaireId => $entry) {
-				$this->writeCsv(
-					PathsFS::fileResponses($studyId, $questionnaireId),
-					"Questionnaire (id=$questionnaireId) does not exist",
-					$entry,
-					$csvSeparator,
-					$successProgressCallback,
-					$errorProgressCallback
-				);
-			}
-		}
-		
-		foreach($cache->getEventCache() as $studyId => $entry) {
-			$this->writeCsv(
-				PathsFS::fileResponses($studyId, PathsFS::FILENAME_EVENTS),
-				"Study $studyId seems to be broken",
-				$entry,
-				$csvSeparator,
-				$successProgressCallback,
-				$errorProgressCallback
-			);
-		}
-		
-		foreach($cache->getFileCache() as $datasetId => $entry) {
-			$path = PathsFS::filePendingUploads($entry->studyId, $userId, $entry->identifier);
-			if(!file_put_contents($path, $entry->internalPath, LOCK_EX))
-				$errorProgressCallback($datasetId, 'Internal Server Error: Saving failed');
-		}
-	}
-	public function uploadFile(int $studyId, string $userId, int $identifier, FileUploader $fileUploader) {
-		$waitingPath = PathsFS::filePendingUploads($studyId, $userId, $identifier);
-		if(!file_exists($waitingPath))
-			throw new CriticalException('Not allowed');
-		
-		$targetPath = file_get_contents($waitingPath);
-		if(!$targetPath)
-			throw new CriticalException('Internal server error');
-		
-		if(file_exists($targetPath))
-			throw new CriticalException('File already exists');
-		
-		if(!$fileUploader->upload($targetPath) || !unlink($waitingPath))
-			throw new CriticalException('Uploading failed');
-		
-		$mediaZipPath = Paths::fileMediaZip($studyId);
-		if(file_exists($mediaZipPath))
-			unlink($mediaZipPath);
-	}
-	
-	public function getLastResponseTimestampOfStudies(): array {
-		$lastActivities = [];
-		$handle = opendir(PathsFS::folderStudies());
-		while($studyId = readdir($handle)) {
-			if($studyId[0] === '.' || $studyId === PathsFS::FILENAME_STUDY_INDEX)
-				continue;
-			
-			$path = PathsFS::fileResponses($studyId, PathsFS::FILENAME_EVENTS);
-			$lastActivities[$studyId] = filemtime($path);
-		}
-		closedir($handle);
-		return $lastActivities;
-	}
-	
-	private function fillMediaFolder(ZipArchive $zip, $path, callable $getMediaFilename, int $startCount, int $totalCount) {
-		$handle = opendir($path);
-		$count = $startCount;
-		while($file = readdir($handle)) {
-			if($file[0] != '.') {
-				$zip->addFile("$path/$file", $getMediaFilename($file));
+    private function fillDatasetErrors(DataSetCacheContainer $entry, $msg, callable $errorCallback) {
+        foreach ($entry->ids as $datasetId) {
+            $errorCallback($datasetId, $msg);
+        }
+    }
+    private function writeDataSetFile(string $path, string $text, DataSetCacheContainer $entry, callable $successCallback, callable $errorCallback) {
+        if (file_put_contents($path, $text, FILE_APPEND | LOCK_EX)) {
+            foreach ($entry->ids as $datasetId) {
+                $successCallback($datasetId);
+            }
+        } else {
+            Main::report("Could not write to file '$path'. Sending error response to app.");
+            $this->fillDatasetErrors($entry, 'Internal Server Error: Saving failed', $errorCallback);
+        }
+    }
+    private function writeCsv(
+        string $path,
+        string $noPathMessage,
+        DataSetCacheContainer $entry,
+        string $csvSeparator,
+        callable $successCallback,
+        callable $errorCallback
+    ) {
+        if (!file_exists($path)) {
+            $this->fillDatasetErrors($entry, $noPathMessage, $errorCallback);
+            return;
+        }
+        
+        $writeString = '';
+        foreach ($entry->data as $data) {
+            $writeString .= "\n".Main::arrayToCSV($data, $csvSeparator);
+        }
+        
+        $this->writeDataSetFile($path, $writeString, $entry, $successCallback, $errorCallback);
+    }
+    
+    public function saveWebAccessDataSet(int $studyId, int $timestamp, string $pageName, string $referer, string $userAgent): bool {
+        return file_put_contents(
+            PathsFS::fileResponses($studyId, PathsFS::FILENAME_WEB_ACCESS),
+            "\n\"".$timestamp."\";\"$pageName\";\"$referer\";\"$userAgent\"",
+            FILE_APPEND | LOCK_EX
+        ) !== false;
+    }
+    public function saveDataSetCache(string $userId, DataSetCache $cache, callable $successProgressCallback, callable $errorProgressCallback) {
+        foreach ($cache->getStatisticsCache() as $studyId => $entry) {
+            $pathStatisticsNewData = PathsFS::fileStatisticsNewData($studyId);
+            
+            $writeString = '';
+            foreach ($entry->data as $data) {
+                $writeString .= "\n" .StatisticsNewDataSetEntryLoader::export($data);
+            }
+            
+            $this->writeDataSetFile($pathStatisticsNewData, $writeString, $entry, $successProgressCallback, $errorProgressCallback);
+        }
+        
+        $csvSeparator = Configs::get('csv_delimiter');
+        foreach ($cache->getQuestionnaireCache() as $studyId => $questionnaireEntries) {
+            foreach ($questionnaireEntries as $questionnaireId => $entry) {
+                $this->writeCsv(
+                    PathsFS::fileResponses($studyId, $questionnaireId),
+                    "Questionnaire (id=$questionnaireId) does not exist",
+                    $entry,
+                    $csvSeparator,
+                    $successProgressCallback,
+                    $errorProgressCallback
+                );
+            }
+        }
+        
+        foreach ($cache->getEventCache() as $studyId => $entry) {
+            $this->writeCsv(
+                PathsFS::fileResponses($studyId, PathsFS::FILENAME_EVENTS),
+                "Study $studyId seems to be broken",
+                $entry,
+                $csvSeparator,
+                $successProgressCallback,
+                $errorProgressCallback
+            );
+        }
+        
+        foreach ($cache->getFileCache() as $datasetId => $entriesArray) {
+            foreach ($entriesArray as $entry) {
+                $path = PathsFS::filePendingUploads($entry->studyId, $userId, $entry->identifier);
+                if (!file_put_contents($path, $entry->internalPath, LOCK_EX))
+                    $errorProgressCallback($datasetId, 'Internal Server Error: Saving failed');
+            }
+        }
+    }
+    public function uploadFile(int $studyId, string $userId, int $identifier, FileUploader $fileUploader) {
+        $waitingPath = PathsFS::filePendingUploads($studyId, $userId, $identifier);
+        if (!file_exists($waitingPath))
+            throw new CriticalException('Not allowed');
+        
+        $targetPath = file_get_contents($waitingPath);
+        if (!$targetPath)
+            throw new CriticalException('Internal server error');
+        
+        if (file_exists($targetPath))
+            throw new CriticalException('File already exists');
+        
+        if (!$fileUploader->upload($targetPath) || !unlink($waitingPath))
+            throw new CriticalException('Uploading failed');
+        
+        $mediaZipPath = Paths::fileMediaZip($studyId);
+        if (file_exists($mediaZipPath))
+            unlink($mediaZipPath);
+    }
+    
+    public function getLastResponseTimestampOfStudies(): array {
+        $lastActivities = [];
+        $handle = opendir(PathsFS::folderStudies());
+        while ($studyId = readdir($handle)) {
+            if ($studyId[0] === '.' || $studyId === PathsFS::FILENAME_STUDY_INDEX)
+                continue;
+            
+            $path = PathsFS::fileResponses($studyId, PathsFS::FILENAME_EVENTS);
+            $lastActivities[$studyId] = filemtime($path);
+        }
+        closedir($handle);
+        return $lastActivities;
+    }
+    
+    private function fillMediaFolder(ZipArchive $zip, $path, callable $getMediaFilename, int $startCount, int $totalCount) {
+        $handle = opendir($path);
+        $count = $startCount;
+        while ($file = readdir($handle)) {
+            if ($file[0] != '.') {
+                $zip->addFile("$path/$file", $getMediaFilename($file));
 
-				$count++;
-				$percent = round(($count / $totalCount) * 100);
-				echo "event: progress\n";
-				echo "data: $percent\n\n";
-				if (ob_get_contents())
-					ob_end_flush();
-				flush();
-			}
-		}
-		closedir($handle);
-	}
-	public function createMediaZip(int $studyId) {
-		$pathZip = Paths::fileMediaZip($studyId);
-		$zip = new ZipArchive();
-		$zip->open($pathZip, ZIPARCHIVE::CREATE);
-		$folderImages = Paths::folderImages($studyId);
-		$folderAudio = Paths::folderAudio($studyId);
-		$countImages = count(scandir($folderImages)) - 2;
-		$countAudio = count(scandir($folderAudio)) - 2;
-		$countTotal = $countImages + $countAudio;
+                $count++;
+                $percent = round(($count / $totalCount) * 100);
+                echo "event: progress\n";
+                echo "data: $percent\n\n";
+                if (ob_get_contents())
+                    ob_end_flush();
+                flush();
+            }
+        }
+        closedir($handle);
+    }
+    public function createMediaZip(int $studyId) {
+        $pathZip = Paths::fileMediaZip($studyId);
+        $zip = new ZipArchive();
+        $zip->open($pathZip, ZIPARCHIVE::CREATE);
+        $folderImages = Paths::folderImages($studyId);
+        $folderAudio = Paths::folderAudio($studyId);
+        $countImages = count(scandir($folderImages)) - 2;
+        $countAudio = count(scandir($folderAudio)) - 2;
+        $countTotal = $countImages + $countAudio;
 
-		echo "event: progress\n";
-		echo "data: 0\n\n";
-		if (ob_get_contents())
-			ob_end_flush();
-		flush();
+        echo "event: progress\n";
+        echo "data: 0\n\n";
+        if (ob_get_contents())
+            ob_end_flush();
+        flush();
 
-		$this->fillMediaFolder(
-			$zip,
-			Paths::folderImages($studyId),
-			function($fileName) { return Paths::publicFileImageFromMediaFilename($fileName); },
-			0,
-			$countTotal
-		);
-		$this->fillMediaFolder(
-			$zip,
-			Paths::folderAudio($studyId),
-			function($fileName) { return Paths::publicFileAudioFromMediaFilename($fileName); },
-			$countImages,
-			$countTotal
-		);
-		
-		$zip->close();
-	}
-	public function outputResponsesFile(int $studyId, string $identifier) {
-		$path = PathsFS::fileResponses($studyId, $identifier);
-		if(!file_exists($path))
-			throw new CriticalException("$path does not exist");
-		
-		Main::setHeader('Content-Type: text/csv');
-		readfile($path);
-	}
-	
-	/**
-	 * @throws CriticalException
-	 */
-	private function outputMediaFromResponses(string $path, string $contentType) {
-		if(!file_exists($path))
-			throw new CriticalException("$path does not exist");
-		
-		Main::setHeader("Content-Type: $contentType");
-		Main::setHeader('Content-Length: '.filesize($path));
-		readfile($path);
-	}
-	public function outputImageFromResponses(int $studyId, string $userId, int $entryId, string $key) {
-		$this->outputMediaFromResponses(Paths::fileImageFromData($studyId, $userId, $entryId, $key), 'image/png');
-	}
-	public function outputAudioFromResponses(int $studyId, string $userId, int $entryId, string $key) {
-		$this->outputMediaFromResponses(Paths::fileAudioFromData($studyId, $userId, $entryId, $key), 'video/3gpp');
-	}
-	public function getResponseFilesList(int $studyId): array {
-		$list = [];
-		$events_file = PathsFS::FILENAME_EVENTS.'.csv';
-		$webAccess_file = PathsFS::FILENAME_WEB_ACCESS.'.csv';
-		$path = opendir(PathsFS::folderResponses($studyId));
-		while($file = readdir($path)) {
-			if($file[0] != '.' && $file != $events_file && $file != $webAccess_file) {
-				$list[] = substr($file, 0, -4);
-			}
-		}
-		return $list;
-	}
-	
+        $this->fillMediaFolder(
+            $zip,
+            Paths::folderImages($studyId),
+            function ($fileName) { return Paths::publicFileImageFromMediaFilename($fileName); },
+            0,
+            $countTotal
+        );
+        $this->fillMediaFolder(
+            $zip,
+            Paths::folderAudio($studyId),
+            function ($fileName) { return Paths::publicFileAudioFromMediaFilename($fileName); },
+            $countImages,
+            $countTotal
+        );
+        
+        $zip->close();
+    }
+    public function outputResponsesFile(int $studyId, string $identifier) {
+        $path = PathsFS::fileResponses($studyId, $identifier);
+        if (!file_exists($path))
+            throw new CriticalException("$path does not exist");
+        
+        Main::setHeader('Content-Type: text/csv');
+        readfile($path);
+    }
+    
+    /**
+     * @throws CriticalException
+     */
+    private function outputMediaFromResponses(string $path, string $contentType) {
+        if (!file_exists($path))
+            throw new CriticalException("$path does not exist");
+        
+        Main::setHeader("Content-Type: $contentType");
+        Main::setHeader('Content-Length: '.filesize($path));
+        readfile($path);
+    }
+    public function outputImageFromResponses(int $studyId, string $userId, int $entryId, string $key) {
+        $this->outputMediaFromResponses(Paths::fileImageFromData($studyId, $userId, $entryId, $key), 'image/png');
+    }
+    public function outputAudioFromResponses(int $studyId, string $userId, int $entryId, string $key) {
+        $this->outputMediaFromResponses(Paths::fileAudioFromData($studyId, $userId, $entryId, $key), 'video/3gpp');
+    }
+    public function getResponseFilesList(int $studyId): array {
+        $list = [];
+        $events_file = PathsFS::FILENAME_EVENTS.'.csv';
+        $webAccess_file = PathsFS::FILENAME_WEB_ACCESS.'.csv';
+        $path = opendir(PathsFS::folderResponses($studyId));
+        while ($file = readdir($path)) {
+            if ($file[0] != '.' && $file != $events_file && $file != $webAccess_file) {
+                $list[] = substr($file, 0, -4);
+            }
+        }
+        return $list;
+    }
+
 }
