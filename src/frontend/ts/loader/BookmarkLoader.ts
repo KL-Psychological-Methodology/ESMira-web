52e689b2dc3dce23a9f56bf638d923ce527bd517
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2025-08-27 14:06:47 +0200

    workaround for crash due to failing to load the bookmarks loader

--- src/frontend/ts/loader/BookmarkLoader.ts
+++ src/frontend/ts/loader/BookmarkLoader.ts
@@ -1,10 +1,10 @@
 import m from "mithril";
-import {PromiseCache} from "../singletons/PromiseCache";
-import {FILE_ADMIN} from "../constants/urls";
-import {Requests} from "../singletons/Requests";
-import {Lang} from "../singletons/Lang";
-import {ObservableStructure, ObservableStructureDataType} from "../observable/ObservableStructure";
-import {ObservableArray} from "../observable/ObservableArray";
+import { PromiseCache } from "../singletons/PromiseCache";
+import { FILE_ADMIN } from "../constants/urls";
+import { Requests } from "../singletons/Requests";
+import { Lang } from "../singletons/Lang";
+import { ObservableStructure, ObservableStructureDataType } from "../observable/ObservableStructure";
+import { ObservableArray } from "../observable/ObservableArray";
 
 
 export class Bookmark extends ObservableStructure {
@@ -14,9 +14,9 @@ export class Bookmark extends ObservableStructure {
 
 export class BookmarkLoader {
 	private bookmarks?: ObservableArray<ObservableStructureDataType, Bookmark>
-	
+
 	public async init(): Promise<BookmarkLoader> {
-		return PromiseCache.get("bookmarsList", async() => {
+		return PromiseCache.get("bookmarsList", async () => {
 			const bookmarksJson = await Requests.loadJson(`${FILE_ADMIN}?type=GetBookmarks`)
 			this.bookmarks = new ObservableArray<ObservableStructureDataType, Bookmark>(
 				bookmarksJson,
@@ -32,38 +32,38 @@ export class BookmarkLoader {
 			return this
 		})
 	}
-	
+
 	private getBookmarkIndex(url: string): number {
 		return this.getBookmarkList().map((bookmark) => {
 			return bookmark.url.get()
 		}).indexOf(url)
 	}
-	
+
 	public isBookmarkListEmpty(): boolean {
-		return this.bookmarks!.get().length == 0
+		return this.bookmarks === undefined ? true : this.bookmarks!.get().length == 0
 	}
-	
+
 	public hasBookmark(url: string): boolean {
 		return this.getBookmarkIndex(url) >= 0
 	}
-	
+
 	public getBookmarkList(): Bookmark[] {
 		return this.bookmarks?.get() ?? []
 	}
-	
+
 	public async deleteBookmark(url: string): Promise<void> {
 		let response = url
 		response = await Requests.loadJson(`${FILE_ADMIN}?type=DeleteBookmark`, "post", `url=${url}`)
-		if(response != url)
+		if (response != url)
 			throw new Error(Lang.get("error_unknown"))
 		this.bookmarks!.remove(this.getBookmarkIndex(url))
 	}
-	
+
 	public async setBookmark(url: string, alias: string): Promise<void> {
 		const bookmarks = this.bookmarks!
 		let bookmarkJson = await Requests.loadJson(`${FILE_ADMIN}?type=SetBookmark`, "post", `url=${url}&alias=${alias}`)
 		const index = this.getBookmarkIndex(url)
-		if(index >= 0)
+		if (index >= 0)
 			bookmarks.get()[index].alias.set(alias)
 		else
 			bookmarks.push(bookmarkJson)
