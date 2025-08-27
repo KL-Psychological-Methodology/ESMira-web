acc5335709ecbc433d028643ccfe26c00aab3058
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2025-08-27 14:02:43 +0200

    fixed publication

--- src/frontend/ts/pages/publications.tsx
+++ src/frontend/ts/pages/publications.tsx
@@ -1,10 +1,10 @@
-import {SectionContent} from "../site/SectionContent";
-import m, {Vnode} from "mithril";
-import {DashRow} from "../widgets/DashRow";
-import {DashElement} from "../widgets/DashElement";
-import {Lang} from "../singletons/Lang";
-import {Section} from "../site/Section";
-import {AboutESMiraLoader, ESMiraPublicationsInterface} from "../loader/AboutESMiraLoader";
+import { SectionContent } from "../site/SectionContent";
+import m, { Vnode } from "mithril";
+import { DashRow } from "../widgets/DashRow";
+import { DashElement } from "../widgets/DashElement";
+import { Lang } from "../singletons/Lang";
+import { Section } from "../site/Section";
+import { AboutESMiraLoader, ESMiraPublicationsInterface } from "../loader/AboutESMiraLoader";
 
 export class Content extends SectionContent {
 	private publications: ESMiraPublicationsInterface
@@ -13,18 +13,18 @@ export class Content extends SectionContent {
 			AboutESMiraLoader.loadPublications()
 		]
 	}
-	
+
 	constructor(section: Section, publications: ESMiraPublicationsInterface) {
 		super(section)
 		this.publications = publications
 	}
-	
+
 	public title(): string {
 		return Lang.get("publications_using_esmira")
 	}
-	
-	
-	
+
+
+
 	public getView(): Vnode<any, any> {
 		return <div>
 			<h2>{Lang.getWithColon("main_publication")}</h2>
@@ -34,16 +34,16 @@ export class Content extends SectionContent {
 						<div>
 							<div class="spacingLeft spacingRight">
 								<p class="hanging">
-									Lewetz, D., Stieger, S. (2023). ESMira: A decentralized open-source application for collecting experience sampling
-									data. <i>Behavior Research Methods</i>.
-									<br/>
+									Lewetz, D., Stieger, S. (2024). ESMira: A decentralized open-source application for collecting experience sampling
+									data. <i>Behavior Research Methods</i>, <i>56</i>, 4421-4434.
+									<br />
 									<a class="showArrow" href="https://doi.org/10.3758/s13428-023-02194-2" target="_blank">https://doi.org/10.3758/s13428-023-02194-2</a>
 								</p>
 							</div>
 						</div>
 				})
 			)}
-			
+
 			{
 				this.publications.years.map((year) => {
 					const publications = this.publications.entries[year]
@@ -55,12 +55,12 @@ export class Content extends SectionContent {
 									<div class="spacingLeft spacingRight">
 										<p class="hanging justify">
 											{!!pub.title && m.trust(pub.title)}
-											<br/>
+											<br />
 											{!!pub.url && <a class="showArrow" href={pub.url} target="_blank">{pub.url}</a>}
 										</p>
 									</div>
 							})
-							)
+						)
 						)}
 					</div>
 				})
