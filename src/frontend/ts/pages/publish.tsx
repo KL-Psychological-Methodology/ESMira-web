1bcc697324e5d65193347fc9b564289357844acd
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2025-09-09 09:18:43 +0200

    fixed citation on publish page

--- src/frontend/ts/pages/publish.tsx
+++ src/frontend/ts/pages/publish.tsx
@@ -179,7 +179,7 @@ export class Content extends SectionContent {
 							<h2>{Lang.getWithColon("plead_to_cite_us")}</h2>
 							<div class="spacingLeft">
 								<p class="hanging">
-									Lewetz, D., Stieger, S. (2023). ESMira: A decentralized open-source application for collecting experience sampling data. <i>Behavior Research Methods</i>.
+									Lewetz, D., Stieger, S. (2024). ESMira: A decentralized open-source application for collecting experience sampling data. <i>Behavior Research Methods</i>, <i>56</i>, 4421-4434.
 									<br />
 									<a class="showArrow" href="https://doi.org/10.3758/s13428-023-02194-2" target="_blank">https://doi.org/10.3758/s13428-023-02194-2</a>
 								</p>
