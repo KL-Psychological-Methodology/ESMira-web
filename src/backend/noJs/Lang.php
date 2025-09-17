2ab4dc0542a05834fdb2ed4597a04828bc298686
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2025-09-17 09:49:59 +0200

    ensured missing strings use default language fallback server side

--- src/backend/noJs/Lang.php
+++ src/backend/noJs/Lang.php
@@ -10,7 +10,13 @@ class Lang {
 	private static $initialized = false;
 	static function init() {
 		self::$langCode = Main::getLang();
-		self::$cache = json_decode(file_get_contents(DIR_BASE .'locales/'.self::$langCode.'.json'));
+		if(self::$langCode == "en") {
+			self::$cache = json_decode(file_get_contents(DIR_BASE .'locales/'.self::$langCode.'.json'));
+		} else {
+			$stringsEn = json_decode(file_get_contents(DIR_BASE .'locales/en.json'));
+			$stringsLang = json_decode(file_get_contents(DIR_BASE .'locales/'.self::$langCode.'.json'));
+			self::$cache = (object) array_merge((array) $stringsEn, (array) $stringsLang);	
+		}
 		self::$initialized = true;
 	}
 	
