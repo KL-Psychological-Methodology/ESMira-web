6cfaf4727eafd0a257212d5184bffeef572befdb
JodliDev <jodlidev@gmail.com>
2025-10-10 09:21:15 +0200

    Add code documentation and clean-up code

--- src/frontend/ts/singletons/Requests.ts
+++ src/frontend/ts/singletons/Requests.ts
@@ -6,16 +6,30 @@ export interface RequestType {
 	post: string
 }
 
+/**
+ * Bundles all backend communication
+ */
 export const Requests = {
-	loadRaw(url: string, type: keyof RequestType = "get", requestData: string = ""): Promise<string> {
-		return new Promise<XMLHttpRequest>((resolve) => {
+	/**
+	 * Loads raw textual data from a specified URL using an XMLHttpRequest.
+	 *
+	 * @param url - The URL from which to fetch the data.
+	 * @param type - The HTTP request type to use (e.g., "get", "post").
+	 * @param requestData - The data to send with the request (if using "post").
+	 * @returns A promise that resolves to the raw response text received from the server.
+	 * @throws error_connection_failed If the creation of the request fails.
+	 */
+	async loadRaw(url: string, type: keyof RequestType = "get", requestData: string = ""): Promise<string> {
+		const request = await new Promise<XMLHttpRequest>((resolve) => {
 			const r = new XMLHttpRequest()
-			if(!r)
+			if(!r) {
 				throw new Error(Lang.get("error_create_request_failed"))
+			}
 			
 			r.open(type, url)
-			if(type == "post")
-				r.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+			if(type == "post") {
+				r.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+			}
 			
 			r.onreadystatechange = function() {
 				if(r.readyState !== 4)
@@ -23,23 +37,32 @@ export const Requests = {
 				resolve(r)
 			}
 			r.send(requestData)
-		}).then((r) => {
-			if(r.status !== 200) {
-				console.error(r)
-				throw new Error(Lang.get("error_connection_failed"))
-			}
-			return r.responseText
 		})
+		if(request.status !== 200) {
+			console.error(request)
+			throw new Error(Lang.get("error_connection_failed"))
+		}
+		return request.responseText
 	},
-	loadJson(url: string, type: keyof RequestType = "get", requestData: string = ""): Promise<any> {
-		return this.loadRaw(url, type, requestData)
-			.then((response) => {
-				const obj = JSON.parse(response)
-				
-				if(obj.success)
-					return obj.dataset
-				else
-					throw Lang.get("error_from_server", obj.error || response)
-			})
+	
+	/**
+	 * Loads and parses a JSON response from a specified URL.
+	 *
+	 * @param url - The URL from which to fetch the JSON data.
+	 * @param type - The HTTP request type to use (e.g., "get", "post").
+	 * @param requestData - The data to send with the request (if using "post").
+	 * @returns A promise that resolves to the dataset from the JSON response.
+	 * @throws error_connection_failed If the creation of the request fails.
+	 * @throws error_from_server if the JSON contains error = true.
+	 */
+	async loadJson(url: string, type: keyof RequestType = "get", requestData: string = ""): Promise<any> {
+		const response = await this.loadRaw(url, type, requestData)
+		const obj = JSON.parse(response)
+		if(obj.success) {
+			return obj.dataset
+		}
+		else {
+			throw Lang.get("error_from_server", obj.error || response)
+		}
 	}
 }
