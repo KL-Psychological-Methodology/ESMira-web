990c0e789f5724fb17fae32e8ceb7d69fe670049
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2025-11-04 14:42:40 +0100

    reworked publish page

--- src/frontend/ts/sections/publish.tsx
+++ src/frontend/ts/sections/publish.tsx
@@ -1,23 +1,23 @@
-import {SectionContent} from "../site/SectionContent";
-import m, {Vnode} from "mithril";
-import {Lang} from "../singletons/Lang";
-import {BindObservable} from "../components/BindObservable";
-import {Study} from "../data/study/Study";
-import {ObservablePrimitive} from "../observable/ObservablePrimitive";
-import {TabBar, TabContent} from "../components/TabBar";
-import {createAppUrl, createFallbackAppUrl, createQuestionnaireUrl, createStudyUrl, safeConfirm} from "../constants/methods";
+import { SectionContent } from "../site/SectionContent";
+import m, { Vnode } from "mithril";
+import { Lang } from "../singletons/Lang";
+import { BindObservable } from "../components/BindObservable";
+import { Study } from "../data/study/Study";
+import { ObservablePrimitive } from "../observable/ObservablePrimitive";
+import { TabBar, TabContent } from "../components/TabBar";
+import { createAppUrl, createFallbackAppUrl, createQuestionnaireUrl, createStudyUrl, safeConfirm } from "../constants/methods";
 import qrcode from "qrcode-generator"
-import {BtnAdd, BtnCopy, BtnCustom, BtnTrash} from "../components/Buttons";
-import {DashRow} from "../components/DashRow";
-import {DashElement, DashViewOptions} from "../components/DashElement";
-import {closeDropdown, openDropdown} from "../components/DropdownMenu";
+import { BtnAdd, BtnCopy, BtnCustom, BtnTrash } from "../components/Buttons";
+import { DashRow } from "../components/DashRow";
+import { DashElement, DashViewOptions } from "../components/DashElement";
+import { closeDropdown, openDropdown } from "../components/DropdownMenu";
 import downloadSvg from "../../imgs/icons/download.svg?raw"
 import studyDesc from "../../imgs/dashIcons/studyDesc.svg?raw"
 import questionSvg from "../../imgs/icons/question.svg?raw"
-import {Requests} from "../singletons/Requests";
-import {FILE_ADMIN, URL_WIKI_DIFFERENCE_LINKS} from "../constants/urls";
+import { Requests } from "../singletons/Requests";
+import { FILE_ADMIN, URL_WIKI_DIFFERENCE_LINKS } from "../constants/urls";
 import warnSvg from "../../imgs/icons/warn.svg?raw";
-import {SectionData} from "../site/SectionData";
+import { SectionData } from "../site/SectionData";
 
 export class Content extends SectionContent {
 	private readonly selectedIndex: ObservablePrimitive<number> = new ObservablePrimitive<number>(0, null, "accessKeyIndex")
@@ -40,8 +40,8 @@ export class Content extends SectionContent {
 		this.fallbackUrls = fallbackUrls
 		this.duplicateAccessKeys = duplicateAccessKeys
 		const study = this.getStudyOrNull()
-		if (study != null && study.accessKeys.get().length > 0) {
-			this.currentUrl = 2
+		if (study != null && study.accessKeys.get().length > 0 && study.publishedWeb.get()) {
+			this.currentUrl = 1
 		}
 	}
 
@@ -124,24 +124,34 @@ export class Content extends SectionContent {
 
 	private getPublishedView(): Vnode<any, any> {
 		const study = this.getStudyOrThrow()
-		return <div class="center">
-			<div>
-				<label class="noTitle noDesc">
-					<input type="radio" name="published" checked={!study.published.get()} onchange={this.changePublishedState.bind(this, false, false)} />
-					{Lang.get("published_not")}
-				</label>
+		return <div>
+			<div class="center">
+				<div>
+					<label class="noTitle noDesc">
+						<input type="radio" name="published" checked={!study.published.get()} onchange={this.changePublishedState.bind(this, false, false)} />
+						{Lang.get("published_not")}
+					</label>
+				</div>
+				<div>
+					<label class="noTitle noDesc">
+						<input type="radio" name="published" checked={study.published.get() && !study.studyOver.get()} onchange={this.changePublishedState.bind(this, true, false)} />
+						{Lang.get("published")}
+					</label>
+				</div>
+				<div>
+					<label class="noTitle noDesc">
+						<input type="radio" name="published" checked={study.published.get() && study.studyOver.get()} onchange={this.changePublishedState.bind(this, true, true)} />
+						{Lang.get("study_over")}
+					</label>
+				</div>
 			</div>
+
 			<div>
-				<label class="noTitle noDesc">
-					<input type="radio" name="published" checked={study.published.get() && !study.studyOver.get()} onchange={this.changePublishedState.bind(this, true, false)} />
-					{Lang.get("published")}
-				</label>
-			</div>
-			<div>
-				<label class="noTitle noDesc">
-					<input type="radio" name="published" checked={study.published.get() && study.studyOver.get()} onchange={this.changePublishedState.bind(this, true, true)} />
-					{Lang.get("study_over")}
-				</label>
+				<ol>
+					<li><b>{Lang.getWithColon("published_not")}</b> {Lang.get("hint_study_unpublished")}</li>
+					<li><b>{Lang.getWithColon("published")}</b> {Lang.get("hint_study_published")}</li>
+					<li><b>{Lang.getWithColon("study_over")}</b> {Lang.get("hint_study_over")}</li>
+				</ol>
 			</div>
 		</div>
 	}
@@ -155,7 +165,18 @@ export class Content extends SectionContent {
 
 	public getView(): Vnode<any, any> {
 		const study = this.getStudyOrThrow()
+		const anyPlatformEnabled = study.publishedAndroid.get() || study.publishedIOS.get() || study.publishedWeb.get()
 		return <div>
+			{DashRow(
+				DashElement("stretched", {
+					template: {
+						icon: m.trust(studyDesc),
+						title: Lang.get("hint_to_best_practices")
+					},
+					href: "https://github.com/KL-Psychological-Methodology/ESMira/wiki/Best-practices-and-common-problems"
+				}),
+
+			)}
 			{this.getPublishedView()}
 			{(study.published.get() || study.accessKeys.get().length != 0) &&
 				<div>
@@ -186,7 +207,7 @@ export class Content extends SectionContent {
 						})
 					)}
 					<br />
-					{study.published.get() &&
+					{study.published.get() && (anyPlatformEnabled &&
 						<div>{
 							study.accessKeys.get().length >= 2
 								? TabBar(this.selectedIndex,
@@ -194,9 +215,21 @@ export class Content extends SectionContent {
 								)
 								: this.getPublishView(study, study.accessKeys.get()[0]?.get() ?? "").view()
 						}</div>
+					) || <div>
+							{DashRow(
+								DashElement("stretched", {
+									highlight: true,
+									content:
+										<div class="center">
+											{Lang.get("warning_all_platforms_disabled")}
+										</div>
+								})
+							)}
+						</div>
 					}
 				</div>
 			}
+			{this.getPublishInfoView(study)}
 			{DashRow(
 				DashElement("stretched", {
 					content:
@@ -215,13 +248,6 @@ export class Content extends SectionContent {
 						</div>
 				}),
 
-				DashElement("stretched", {
-					template: {
-						icon: m.trust(studyDesc),
-						title: Lang.get("hint_to_best_practices")
-					},
-					href: "https://github.com/KL-Psychological-Methodology/ESMira/wiki/Best-practices-and-common-problems"
-				}),
 
 				DashElement("stretched", {
 					template: {
@@ -278,34 +304,25 @@ export class Content extends SectionContent {
 		const fallbackUrl = usesFallback ? this.fallbackUrls[0] : ""
 		const fallbackAppInstallUrl = createFallbackAppUrl(accessKey, study.id.get(), fallbackUrl)
 		const hasAccessKeys = accessKey.length > 0
+		const publishedWeb = study.publishedWeb.get()
+		const publishedSmartphone = study.publishedAndroid.get() || study.publishedIOS.get()
 
 		return [
-			{
-				content: <div>
-					<h2>{Lang.getWithColon(hasAccessKeys ? "urls_access_key" : "urls_id")} <a href={URL_WIKI_DIFFERENCE_LINKS} class="right" target="_blank">{BtnCustom(m.trust(questionSvg))}</a></h2>
-					<div class="line">
-						{this.getUrlViewAndCacheUrl(infoTitle, createStudyUrl(accessKey, study.id.get(), false, "https"))}
-					</div>
-					<div class="line">
-						{this.getUrlViewAndCacheUrl(appInstrTitle, createAppUrl(accessKey, study.id.get(), false, "https", fallbackUrl))}
-					</div>
-					{accessKey.length > 0 &&
-						<div class="smallText">
-							{this.duplicateAccessKeys.length > 0 && <div class="inlineIcon">{m.trust(warnSvg)}</div>}
-							{Lang.get("info_urls_without_study_id")}
-						</div>
-					}
-				</div>
-			},
 			hasAccessKeys && {
 				content: <div>
 					<h2>{Lang.getWithColon("urls_id")} <a href={URL_WIKI_DIFFERENCE_LINKS} class="right" target="_blank">{BtnCustom(m.trust(questionSvg))}</a></h2>
-					<div class="line">
-						{this.getUrlViewAndCacheUrl(infoTitle, createStudyUrl(accessKey, study.id.get(), true, "https"))}
-					</div>
-					<div class="line">
-						{this.getUrlViewAndCacheUrl(appInstrTitle, createAppUrl(accessKey, study.id.get(), true, "https", fallbackUrl))}
-					</div>
+					{
+						publishedWeb &&
+						<div class="line">
+							{this.getUrlViewAndCacheUrl(infoTitle, createStudyUrl(accessKey, study.id.get(), true, "https"))}
+						</div>
+					}
+					{
+						publishedSmartphone &&
+						<div class="line">
+							{this.getUrlViewAndCacheUrl(appInstrTitle, createAppUrl(accessKey, study.id.get(), true, "https", fallbackUrl))}
+						</div>
+					}
 					{accessKey.length > 0 &&
 						<div class="smallText">
 							{Lang.get("info_links_with_study_id")}
@@ -313,7 +330,40 @@ export class Content extends SectionContent {
 					}
 				</div>
 			},
-			study.questionnaires.get().length > 0 && {
+			{
+				content: <div>
+					<h2>{Lang.getWithColon(hasAccessKeys ? "urls_access_key" : "urls_id")} <a href={URL_WIKI_DIFFERENCE_LINKS} class="right" target="_blank">{BtnCustom(m.trust(questionSvg))}</a></h2>
+					{
+						publishedWeb &&
+						<div class="line">
+							{this.getUrlViewAndCacheUrl(infoTitle, createStudyUrl(accessKey, study.id.get(), false, "https"))}
+						</div>
+					}
+					{
+						publishedSmartphone &&
+						<div class="line">
+							{this.getUrlViewAndCacheUrl(appInstrTitle, createAppUrl(accessKey, study.id.get(), false, "https", fallbackUrl))}
+						</div>
+					}
+					{accessKey.length > 0 &&
+						<div class="smallText">
+							{this.duplicateAccessKeys.length > 0 && <div class="inlineIcon">{m.trust(warnSvg)}</div>}
+							{Lang.get("info_urls_without_study_id")}
+						</div>
+					}
+					{usesFallback &&
+						<div
+							onpointerenter={this.onPointerEnterUrl.bind(null, fallbackAppInstallUrl)}
+							onpointerleave={this.onPointerLeaveUrl.bind(null)}
+						>
+							<label class="noTitle noDesc">{Lang.get("fallback_app_installation_instructions")}</label>
+							&nbsp;
+							<span class="middle">{BtnCopy(() => navigator.clipboard.writeText(fallbackAppInstallUrl))}</span>
+						</div>
+					}
+				</div>
+			},
+			publishedWeb && study.questionnaires.get().length > 0 && {
 				content: <div>
 					<h2>{Lang.getWithColon("questionnaires")}</h2>
 					{study.questionnaires.get().map((questionnaire) =>
@@ -322,19 +372,28 @@ export class Content extends SectionContent {
 						</div>
 					)}
 				</div>
-			},
-			usesFallback && {
-				content: <div
-					onpointerenter={this.onPointerEnterUrl.bind(null, fallbackAppInstallUrl)}
-					onpointerleave={this.onPointerLeaveUrl.bind(null)}
-				>
-					<h2>{Lang.getWithColon("fallback_app_installation_instructions")}</h2>
-					&nbsp;
-					<span class="middle">
-						{BtnCopy(() => navigator.clipboard.writeText(fallbackAppInstallUrl))}
-					</span>
-				</div>
 			}
 		]
+	}
+
+	private getPublishInfoView(study: Study): Vnode<any, any> {
+		const usesFallback = study.useFallback.get() && this.fallbackUrls.length > 0
+		const smartphoneAndWeb = (study.publishedAndroid.get() || study.publishedIOS.get()) && study.publishedWeb.get()
+
+		return <div>
+			{DashRow(
+				DashElement("stretched", {
+					content:
+						<div>
+							<h2>{Lang.get("publish_info_title")}</h2>
+							<p>{Lang.get("publish_info_text_based")}
+								{usesFallback && Lang.get("publish_info_fallback_link")}
+								{Lang.get("publish_info_flyer_based")}</p>
+							{smartphoneAndWeb && <p>{Lang.get("publish_info_difference_links")}</p>}
+							<p>{Lang.get("publish_info_explanation_qr")}</p>
+						</div>
+				})
+			)}
+		</div>
 	}
 }
