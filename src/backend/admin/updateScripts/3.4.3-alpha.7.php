3bde009e29bf0fc0be11deaf6eb8421e323f9133
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2026-01-27 15:11:06 +0100

    3.4.3-alpha.7

---
+++ src/backend/admin/updateScripts/3.4.3-alpha.7.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+Reason for update:
+	3.4.3 extracts rewards to their own permission. In order to preserve backwards compatibility, the following steps need to be taken:
+		1. update metadata of all studies, so the metadata includes information on whether the reward system is active.
+		2. assign reward permission to all users who previously held read permission for a study.
+*/
+
+
+// 1. Update Metadata
+
+use backend\Configs;
+
+$studyStore = Configs::getDataStore()->getStudyStore();
+$studies = $studyStore->getStudyIdList();
+
+foreach($studies as $studyId) {
+	$study = $studyStore->getStudyConfig($studyId);
+
+	$studyMetadataStore = Configs::getDataStore()->getStudyMetadataStore($studyId);
+	$studyMetadataStore->updateMetadata($study);
+}
+
+// 2. Assign Reward Permissions
+
+$accountStore = Configs::getDataStore()->getAccountStore();
+$accountList = $accountStore->getAccountList();
+
+foreach($accountList as $account) {
+	$permissions = $accountStore->getPermissions($account);
+	$readPermissions = isset($permissions['reward']) ? $permissions['reward'] : [];
+	foreach($readPermissions as $studyId) {
+		$accountStore->addStudyPermission($account, $studyId, 'reward');
+	}
+}
