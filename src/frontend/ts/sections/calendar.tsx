b6770f4422595e6a0f3f130ddf046b35ebe49d10
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2026-01-28 14:57:14 +0100

    fixed title of calendar section

--- src/frontend/ts/sections/calendar.tsx
+++ src/frontend/ts/sections/calendar.tsx
@@ -1,28 +1,28 @@
-import {SectionContent} from "../site/SectionContent";
-import m, {Component, Vnode, VnodeDOM} from "mithril";
-import {Lang} from "../singletons/Lang";
-import {Study} from "../data/study/Study";
-import {Calendar, EventInput} from "@fullcalendar/core";
+import { SectionContent } from "../site/SectionContent";
+import m, { Component, Vnode, VnodeDOM } from "mithril";
+import { Lang } from "../singletons/Lang";
+import { Study } from "../data/study/Study";
+import { Calendar, EventInput } from "@fullcalendar/core";
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import deLocale from '@fullcalendar/core/locales/de';
 import ukLocale from '@fullcalendar/core/locales/uk';
 import calendarSvg from "../../imgs/icons/calendar.svg?raw"
-import {DashRow} from "../components/DashRow";
-import {DashElement} from "../components/DashElement";
-import {BindObservable, DateTransformer, TimeTransformer} from "../components/BindObservable";
-import {ObservablePrimitive} from "../observable/ObservablePrimitive";
-import {BaseObservable, ObserverId} from "../observable/BaseObservable";
-import {Questionnaire} from "../data/study/Questionnaire";
-import {getChartColor} from "../helpers/ChartJsBox";
-import {Scheduler} from "../helpers/Scheduler";
-import {LoadingSpinner} from "../components/LoadingSpinner";
-import {getMidnightMillis} from "../constants/methods";
-import {ActionTrigger} from "../data/study/ActionTrigger";
-import {BtnReload} from "../components/Buttons";
-import {TitleRow} from "../components/TitleRow";
-import {SectionData} from "../site/SectionData";
+import { DashRow } from "../components/DashRow";
+import { DashElement } from "../components/DashElement";
+import { BindObservable, DateTransformer, TimeTransformer } from "../components/BindObservable";
+import { ObservablePrimitive } from "../observable/ObservablePrimitive";
+import { BaseObservable, ObserverId } from "../observable/BaseObservable";
+import { Questionnaire } from "../data/study/Questionnaire";
+import { getChartColor } from "../helpers/ChartJsBox";
+import { Scheduler } from "../helpers/Scheduler";
+import { LoadingSpinner } from "../components/LoadingSpinner";
+import { getMidnightMillis } from "../constants/methods";
+import { ActionTrigger } from "../data/study/ActionTrigger";
+import { BtnReload } from "../components/Buttons";
+import { TitleRow } from "../components/TitleRow";
+import { SectionData } from "../site/SectionData";
 
 interface FullcalendarComponentOptions {
 	study: Study
@@ -43,31 +43,31 @@ class FullcalendarComponent implements Component<FullcalendarComponentOptions, a
 	private colors: Record<number, string> = {}
 	private isLoading: boolean = false
 	private onError: (msg: string) => void = (msg) => console.error(msg)
-	
+
 	private getJoinTimestamp(): number {
 		return getMidnightMillis(this.joinDate.get()) + this.joinTime.get()
 	}
-	
+
 	private calcQuestionnaireStart(questionnaire: Questionnaire): number {
 		const joinDate = this.getJoinTimestamp()
-		
-		if(questionnaire.durationStart.get() != 0)
+
+		if (questionnaire.durationStart.get() != 0)
 			return questionnaire.durationStart.get()
-		else if(questionnaire.durationStartingAfterDays.get() != 0)
+		else if (questionnaire.durationStartingAfterDays.get() != 0)
 			return joinDate + ONE_DAY * questionnaire.durationStartingAfterDays.get()
 		else
 			return joinDate
 	}
 	private calcQuestionnaireEnd(questionnaire: Questionnaire): number | null {
 		const joinTimestamp = this.getJoinTimestamp()
-		if(questionnaire.durationEnd.get() != 0)
+		if (questionnaire.durationEnd.get() != 0)
 			return questionnaire.durationEnd.get()
-		else if(questionnaire.durationPeriodDays.get() != 0)
+		else if (questionnaire.durationPeriodDays.get() != 0)
 			return joinTimestamp + ONE_DAY * (questionnaire.durationPeriodDays.get())
 		else
 			return null
 	}
-	
+
 	private getQuestionnaireEvent(questionnaire: Questionnaire): EventInput {
 		return {
 			title: questionnaire.getTitle(),
@@ -77,9 +77,9 @@ class FullcalendarComponent implements Component<FullcalendarComponentOptions, a
 			allDay: true,
 		}
 	}
-	
+
 	private getActionName(actionTrigger: ActionTrigger): string {
-		switch(actionTrigger.actions.get()[0].type.get()) {
+		switch (actionTrigger.actions.get()[0].type.get()) {
 			case 1:
 				return "[invitation]"
 			case 2:
@@ -90,9 +90,9 @@ class FullcalendarComponent implements Component<FullcalendarComponentOptions, a
 				return "[unknown]"
 		}
 	}
-	
+
 	private addEventsFromScheduler(events: EventInput[]) {
-		for(let alarm of this.scheduler.alarms) {
+		for (let alarm of this.scheduler.alarms) {
 			events.push({
 				title: this.getActionName(alarm.actionTrigger),
 				icon: m.trust(calendarSvg),
@@ -101,47 +101,47 @@ class FullcalendarComponent implements Component<FullcalendarComponentOptions, a
 			})
 		}
 	}
-	
+
 	private collectScheduleEvents(questionnaire: Questionnaire) {
 		const joined = this.getJoinTimestamp()
 		let start = this.getJoinTimestamp()
-		for(const actionTrigger of questionnaire.actionTriggers.get()) {
-			if(actionTrigger.schedules.get().length == 0)
+		for (const actionTrigger of questionnaire.actionTriggers.get()) {
+			if (actionTrigger.schedules.get().length == 0)
 				continue
 			const schedule = actionTrigger.schedules.get()[0]
 			const initialDelay = schedule.getInitialDelayDays()
-			for(let signalTime of schedule.signalTimes.get()) {
+			for (let signalTime of schedule.signalTimes.get()) {
 				this.scheduler.scheduleSignalTime(joined, questionnaire, schedule, signalTime, actionTrigger, start, initialDelay)
 			}
 		}
-		
+
 		this.scheduler.scheduleAheadJavascript(joined)
 	}
-	
+
 	private initCalendar() {
-		if(!this.calendarView)
+		if (!this.calendarView)
 			return
 		this.isLoading = true
 		m.redraw()
 		this.scheduler = new Scheduler()
 		const events: EventInput[] = []
-		
+
 		try {
-			for(const qId in this.visibleCalendars) {
+			for (const qId in this.visibleCalendars) {
 				const questionnaire = this.visibleCalendars[qId]
 				events.push(this.getQuestionnaireEvent(questionnaire))
 				this.collectScheduleEvents(questionnaire)
 			}
 		}
-		catch(e: any) {
+		catch (e: any) {
 			this.onError(e.message || e)
 			this.isLoading = false
 			return
 		}
 		this.addEventsFromScheduler(events)
-		
+
 		this.calendar = new Calendar(this.calendarView, {
-			plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ],
+			plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
 			locales: [deLocale, ukLocale],
 			locale: Lang.code,
 			initialView: this.calendar?.view.type ?? "dayGridMonth",
@@ -154,7 +154,7 @@ class FullcalendarComponent implements Component<FullcalendarComponentOptions, a
 			allDayText: "",
 			editable: false,
 			dayMaxEvents: false,
-			
+
 			events: events,
 			eventTimeFormat: {
 				hour: "2-digit",
@@ -162,39 +162,39 @@ class FullcalendarComponent implements Component<FullcalendarComponentOptions, a
 				hour12: false
 			}
 		})
-		
-		
+
+
 		this.calendar.render()
-		
+
 		this.isLoading = false
 		m.redraw()
 	}
-	
+
 	private toggleQuestionnaire(questionnaire: Questionnaire, e: EventInput) {
 		const target = e.target as HTMLInputElement
-		
-		if(target.checked) {
+
+		if (target.checked) {
 			this.visibleCalendars[questionnaire.internalId.get()] = questionnaire
 		}
 		else
 			delete this.visibleCalendars[questionnaire.internalId.get()]
-		
+
 		this.initCalendar()
 	}
-	
+
 	public oncreate(vNode: VnodeDOM<FullcalendarComponentOptions, any>): void {
 		this.onError = vNode.attrs.onError
 		const study = vNode.attrs.study
 		const joinTimestamp = vNode.attrs.joinTimestamp
 		this.joinDate.set(joinTimestamp.get())
 		this.joinTime.set(joinTimestamp.get() - getMidnightMillis(joinTimestamp.get()))
-		
+
 		study.questionnaires.get().forEach((questionnaire, index) => {
-			if(questionnaire.hasSchedules())
+			if (questionnaire.hasSchedules())
 				this.visibleCalendars[questionnaire.internalId.get()] = questionnaire
 			this.colors[questionnaire.internalId.get()] = getChartColor(index)
 		})
-		
+
 		this.joinDate.addObserver(() => {
 			joinTimestamp.set(this.getJoinTimestamp())
 			this.initCalendar()
@@ -214,10 +214,10 @@ class FullcalendarComponent implements Component<FullcalendarComponentOptions, a
 		this.calendarView = vNode.dom.getElementsByClassName("calendarView")[0] as HTMLElement
 		this.calendar?.updateSize()
 	}
-	
+
 	public view(vNode: Vnode<FullcalendarComponentOptions, any>): Vnode<any, any> {
 		const study = vNode.attrs.study
-		
+
 		return <div>
 			{DashRow(
 				DashElement("stretched", {
@@ -225,20 +225,20 @@ class FullcalendarComponent implements Component<FullcalendarComponentOptions, a
 						<div class="center">
 							<label class="noDesc middle">
 								<small>{Lang.get("join_date")}</small>
-								<input type="date" {... BindObservable(this.joinDate, DateTransformer)}/>
+								<input type="date" {...BindObservable(this.joinDate, DateTransformer)} />
 							</label>
 							<label class="noDesc middle">
 								<small>{Lang.get("join_time")}</small>
-								<input type="time" {... BindObservable(this.joinTime, TimeTransformer)}/>
+								<input type="time" {...BindObservable(this.joinTime, TimeTransformer)} />
 							</label>
 							<div class="horizontal middle">
-								{ BtnReload(() => {
+								{BtnReload(() => {
 									this.initCalendar()
 								})}
 							</div>
 						</div>
-						<br/>
-						<br/>
+						<br />
+						<br />
 						<div class="smallText spacingLeft spacingRight">{Lang.get("calendar_description")}</div>
 					</div>
 				}),
@@ -249,19 +249,19 @@ class FullcalendarComponent implements Component<FullcalendarComponentOptions, a
 							<div>
 								<label class="noTitle noDesc line">
 									<input checked={this.visibleCalendars.hasOwnProperty(questionnaire.internalId.get())} type="checkbox"
-										   onchange={this.toggleQuestionnaire.bind(this, questionnaire)}/>
+										onchange={this.toggleQuestionnaire.bind(this, questionnaire)} />
 									<span style={`color: ${this.colors[questionnaire.internalId.get()]}`}>{questionnaire.getTitle()}</span>
 								</label>
 								<table class="spacingLeft smallText">
 									<tbody>
-									<tr>
-										<td><b>{Lang.getWithColon("from")}</b></td>
-										<td>{new Date(this.calcQuestionnaireStart(questionnaire)).toLocaleString()}</td>
-									</tr>
-									<tr>
-										<td><b>{Lang.getWithColon("to")}</b></td>
-										<td>{endTimeStamp ? new Date(endTimeStamp).toLocaleString() : "∞"}</td>
-									</tr>
+										<tr>
+											<td><b>{Lang.getWithColon("from")}</b></td>
+											<td>{new Date(this.calcQuestionnaireStart(questionnaire)).toLocaleString()}</td>
+										</tr>
+										<tr>
+											<td><b>{Lang.getWithColon("to")}</b></td>
+											<td>{endTimeStamp ? new Date(endTimeStamp).toLocaleString() : "∞"}</td>
+										</tr>
 									</tbody>
 								</table>
 							</div>
@@ -275,7 +275,7 @@ class FullcalendarComponent implements Component<FullcalendarComponentOptions, a
 			</div>
 		</div>
 	}
-	
+
 	public onremove(): void {
 		this.studyObserverId?.removeObserver()
 	}
@@ -286,13 +286,13 @@ export class Content extends SectionContent {
 		return [sectionData.getStudyPromise()]
 	}
 	public title(): string {
-		return Lang.get("study_description")
+		return Lang.get("calendar")
 	}
-	
+
 	public getView(): Vnode<any, any> {
 		const study = this.getStudyOrThrow()
-		
-		if(!study.questionnaires.get().length)
+
+		if (!study.questionnaires.get().length)
 			return <div class="center spacingTop">{Lang.get("info_no_questionnaires_created")}</div>
 		else
 			return m(FullcalendarComponent, {
